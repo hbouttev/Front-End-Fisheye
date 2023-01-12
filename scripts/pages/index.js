d5230906cd00b3f4e0958f2a026c67e399431ce9
Hadrien Boutteville <h.boutteville@leosidea.com>
2023-01-12 10:43:17 +0100

    fix: clean indentation and remove useless semicolons

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,26 +1,26 @@
-    async function getPhotographers() {
-        return await fetch("../../data/photographers.json").then((response) => {
-            return response.json();
-        }).then((data) => {
-            return data;
-        });
-    }
+async function getPhotographers() {
+    return await fetch("../../data/photographers.json").then((response) => {
+        return response.json();
+    }).then((data) => {
+        return data;
+    });
+}
 
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
+async function displayData(photographers) {
+    const photographersSection = document.querySelector(".photographer_section");
 
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerFactory(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    };
+    photographers.forEach((photographer) => {
+        const photographerModel = photographerFactory(photographer);
+        const userCardDOM = photographerModel.getUserCardDOM();
+        photographersSection.appendChild(userCardDOM);
+    });
+}
 
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    };
+async function init() {
+    // Récupère les datas des photographes
+    const {photographers} = await getPhotographers();
+    displayData(photographers);
+}
 
-    init();
+init();
     
