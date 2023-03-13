0512ca5a023871107db2eacefefd1a7c1757aeb6
Hadrien Boutteville <h.boutteville@leosidea.com>
2023-03-13 22:22:16 +0100

    fix: move api calls to api.js

---
+++ scripts/utils/api.js
@@ -0,0 +1,24 @@
+// local cache for fetched data. Would be better in a module scope.
+let cachedPhotographers = null;
+
+async function getPhotographers() {
+    if (cachedPhotographers !== null) {
+        return cachedPhotographers;
+    }
+    return await fetch("../../data/photographers.json").then((response) => {
+        return response.json();
+    }).then((data) => {
+        cachedPhotographers = data;
+        return data;
+    });
+}
+
+async function getPhotographerById(id) {
+    const { photographers } = await getPhotographers();
+    return photographers.find(photographer => photographer.id === id);
+}
+
+async function getMediaByPhotographerId(id) {
+    const { media } = await getPhotographers();
+    return media.filter(media => media.photographerId === id);
+}
--- scripts/utils/getPhotographers.js
+++
@@ -1,7 +0,0 @@
-async function getPhotographers() {
-    return await fetch("../../data/photographers.json").then((response) => {
-        return response.json();
-    }).then((data) => {
-        return data;
-    });
-}
